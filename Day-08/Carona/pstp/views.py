43fbe35bce1f11a8e9bacb1bdd7a516f4a9cbb6f
maneesha <maneeshakokkanti@gmail.com>
2021-03-31 11:20:09 +0530

    first

---
+++ Day-08/Carona/pstp/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def home(request):
+	return HttpResponse("<h2 style='color:white;background-color:green'>Welcome to Home Page</h2>")
+
+
+def chk(request):
+	return HttpResponse("<script>alert('hi good afternoon')</script><h2>welcome</h2>")
+
+
+def homepage(request):
+	return render(request,'rm/homepage.html')
+
+def lgn(re):
+	return render(re,'rm/login1.html')
+
+def reg(rt):
+	return render(rt,'rm/register.html')
